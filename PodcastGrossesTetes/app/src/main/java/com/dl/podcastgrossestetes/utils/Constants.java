9e0cff2d377f56848f4e661c4a5723560f1808a5
LeoLogeart <leo.logeart@gmail.com>
2017-11-14 04:51:08 -0800

    Shorten animation duration

--- PodcastGrossesTetes/app/src/main/java/com/dl/podcastgrossestetes/utils/Constants.java
+++ PodcastGrossesTetes/app/src/main/java/com/dl/podcastgrossestetes/utils/Constants.java
@@ -2,9 +2,9 @@ package com.dl.podcastgrossestetes.utils;
 
 public class Constants {
     public static final int DURATION_MILLIS_FADE = 300;
-    public static final int START_OFFSET_FADE = 300;
-    public static final int EXPAND_DURATION = 500;
-    public static final int START_OFFSET_EXPAND = 500;
+    public static final int START_OFFSET_FADE = 200;
+    public static final int EXPAND_DURATION = 250;
+    public static final int START_OFFSET_EXPAND = 200;
     public static final int ANIMATION_DURATION = EXPAND_DURATION + START_OFFSET_EXPAND;
     public static final int UPDATE_DELAY_MILLIS = 100;
     public static final int NOTIFICATION_ID = 534;
